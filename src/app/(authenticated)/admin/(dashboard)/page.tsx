7aed8f1e807734793017ae2313e19a64f8d09a8a
Orekidesu <cmcodog10@gmail.com>
2025-02-22 01:34:22 +0800

    made a custom component for the mission and vision card

--- src/app/(authenticated)/admin/(dashboard)/page.tsx
+++ src/app/(authenticated)/admin/(dashboard)/page.tsx
@@ -5,7 +5,7 @@ import useMissions from "@/hooks/admin/useMission";
 import useGraduateAttributes from "@/hooks/admin/useGraduateAttribute";
 import { useEffect } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
-import Image from "next/image";
+import VisionMissionCard from "@/components/VisionMissionCard";
 import missionLogo from "../../../../../public/assets/images/mission.png";
 import visionLogo from "../../../../../public/assets/images/vision.png";
 import {
@@ -43,44 +43,22 @@ const Dashboardpage = () => {
   return (
     <div className="grid grid-rows-1 content-center">
       <div className="flex flex-col md:flex-row justify-evenly gap-4">
-        {visionsLoading && missionsLoading ? (
-          <Skeleton className="w-full h-52" />
-        ) : (
-          <Card className="w-full text-center border-2 shadow-md ">
-            <CardHeader className="text-4xl font-semi-bold">
-              <span className="flex items-center justify-center gap-2">
-                Vision
-                <Image
-                  className="h-10 w-10"
-                  src={visionLogo}
-                  alt="vision logo"
-                  priority
-                />
-              </span>
-            </CardHeader>
-            <CardContent className="italic">
-              {visions[0]?.description}
-            </CardContent>
-          </Card>
-        )}
-        {missionsLoading && visionsLoading ? (
-          <Skeleton className="w-full h-52" />
-        ) : (
-          <Card className="w-full text-center border-2 shadow-md ">
-            <CardHeader className="text-4xl font-semi-bold">
-              <span className="flex items-center justify-center gap-2">
-                Mission
-                <Image
-                  className="h-8 w-8"
-                  src={missionLogo}
-                  alt="vision logo"
-                  priority
-                />
-              </span>
-            </CardHeader>
-            <CardContent className="italic">{concatenatedMissions}</CardContent>
-          </Card>
-        )}
+        <VisionMissionCard
+          title="Vision"
+          content={visions[0]?.description || ""}
+          imageSrc={visionLogo}
+          imageAlt="vision logo"
+          imageSize="h-10 w-10"
+          loading={visionsLoading}
+        />
+        <VisionMissionCard
+          title="Mission"
+          content={concatenatedMissions || ""}
+          imageSrc={missionLogo}
+          imageAlt="mission logo"
+          imageSize="h-8 w-8"
+          loading={missionsLoading}
+        />
       </div>
     </div>
   );
