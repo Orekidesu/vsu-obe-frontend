1d6b4f4d122a4032970de8c12ccd83b2455380a2
Orekidesu <cmcodog10@gmail.com>
2025-02-23 14:46:24 +0800

    optimize the sidebar for better ux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex h-screen w-screen overflow-hidden">
-          <main className="w-full grow">
+          <main className="w-full h-full grow">
             <Provider session={session}>{children}</Provider>
           </main>
           <Toaster />
