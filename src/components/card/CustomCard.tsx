eec18669a736c303a060f145d2134b660e613e53
Orekidesu <cmcodog10@gmail.com>
2025-02-24 23:43:57 +0800

    allow custom card to accept optional height and width for the icon

--- src/components/card/CustomCard.tsx
+++ src/components/card/CustomCard.tsx
@@ -5,15 +5,27 @@ interface CardProps {
   title: string;
   icon: string;
   children: ReactNode;
+  iconHeight?: string;
+  iconWidth?: string;
 }
 
-const CustomCard: React.FC<CardProps> = ({ title, icon, children }) => {
+const CustomCard: React.FC<CardProps> = ({
+  title,
+  icon,
+  children,
+  iconHeight = "h-10",
+  iconWidth = "w-10",
+}) => {
   return (
     <Card className="w-full text-center border-2 shadow-md">
       <CardHeader className="text-4xl font-semi-bold">
         <span className="flex items-center justify-center gap-2">
           {title}
-          <img src={icon} alt={title} className="h-10 w-10" />
+          <img
+            src={icon}
+            alt={title}
+            className={`${iconHeight} ${iconWidth}`}
+          />
         </span>
       </CardHeader>
       <CardContent className="italic">{children}</CardContent>
