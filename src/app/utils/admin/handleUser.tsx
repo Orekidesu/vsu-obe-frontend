f2dd89ba89c62d15f8579822a20be125fa477690
Orekidesu <cmcodog10@gmail.com>
2025-03-10 04:38:26 +0800

    added a handler for updating user

--- src/app/utils/admin/handleUser.tsx
+++ src/app/utils/admin/handleUser.tsx
@@ -2,6 +2,7 @@ import { User } from "@/types/model/User";
 import { UseMutationResult } from "@tanstack/react-query";
 import { rejects } from "assert";
 import { set } from "react-hook-form";
+import { number } from "zod";
 
 export const createUserHandler = async (
   createUserMutation: UseMutationResult<any, any, Partial<User>, unknown>,
@@ -24,5 +25,41 @@ export const createUserHandler = async (
         resolve();
       },
     });
+  });
+};
+
+export const updateUserHandler = async (
+  updateUserMutation: UseMutationResult<
+    any,
+    any,
+    { id: number; updatedData: Partial<User> },
+    unknown
+  >,
+  data: Partial<User>,
+  setFormError: (error: Record<string, string[]> | string | null) => void
+) => {
+  return new Promise<void>((resolve, reject) => {
+    if (data.id) {
+      updateUserMutation.mutate(
+        { id: data.id, updatedData: data },
+        {
+          onError: (error: any) => {
+            setFormError(
+              error?.response?.data?.errors ||
+                "Something went wrong while updating the user."
+            );
+            reject(
+              new Error(
+                error?.response?.data?.errors || "Failed to update user"
+              )
+            );
+          },
+          onSuccess: () => {
+            setFormError(null);
+            resolve();
+          },
+        }
+      );
+    }
   });
 };
