c7a041f2d4e193606377e236039805fa5509fd10
Orekidesu <cmcodog10@gmail.com>
2025-04-08 18:34:27 +0800

    add New Program page: create a new component for the New Program page layout

---
+++ src/app/(authenticated)/department/proposals/new-program/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+const NewProgramPage = () => {
+  return (
+    <div className="flex-col justify-center align-middle h-full w-full">
+      <div className="grow-">1</div>
+      <div>2</div>
+    </div>
+  );
+};
+
+export default NewProgramPage;
