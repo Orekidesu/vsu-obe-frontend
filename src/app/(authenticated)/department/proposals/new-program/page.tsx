3b9e6eb28a3218851a1f21b1fd9509f5850c1f9e
Orekidesu <cmcodog10@gmail.com>
2025-04-09 10:56:30 +0800

    add radio group component: implement Radix UI radio group with custom styling

--- src/app/(authenticated)/department/proposals/new-program/page.tsx
+++ src/app/(authenticated)/department/proposals/new-program/page.tsx
@@ -1,10 +1,10 @@
 "use client";
+import WizardForm from "@/components/department-components/WizardForm";
 import React from "react";
 const NewProgramPage = () => {
   return (
     <div className="flex justify-center items-center h-full">
-      <div>1</div>
-      <div>2</div>
+      <WizardForm />
     </div>
   );
 };
