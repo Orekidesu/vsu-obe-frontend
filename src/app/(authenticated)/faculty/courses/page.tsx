d74a87df2e81bd38debfd40b22346250ffc9c753
Orekidesu <cmcodog10@gmail.com>
2025-04-30 18:34:26 +0800

    feat: create MyCoursesPage component to display course tabs for authenticated faculty

---
+++ src/app/(authenticated)/faculty/courses/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { CourseTabs } from "@/components/committee-components/CourseTabs";
+
+export default function MyCoursesPage() {
+  return (
+    <div className="container py-10">
+      <CourseTabs />
+    </div>
+  );
+}
