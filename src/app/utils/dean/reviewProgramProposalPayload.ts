05f7b975dc4dd213793c138bd09fb790947d2cc4
Orekidesu <cmcodog10@gmail.com>
2025-05-15 11:58:13 +0800

    fix: update ReviewProposalPayload interface to include section in committee_level for better structure

--- src/app/utils/dean/reviewProgramProposalPayload.ts
+++ src/app/utils/dean/reviewProgramProposalPayload.ts
@@ -1,5 +1,9 @@
 export interface ReviewProposalPayload {
   status: string;
   department_level?: Array<{ section: string; details: string }>;
-  committee_level?: Array<{ curriculum_course_id: number; details: string }>;
+  committee_level?: Array<{
+    curriculum_course_id: number;
+    section: string;
+    details: string;
+  }>;
 }
