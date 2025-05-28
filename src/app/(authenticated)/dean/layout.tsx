2bd8a5319fab7be7b12dd0c1b2cec60e7f054d39
Orekidesu <cmcodog10@gmail.com>
2025-05-28 10:26:50 +0800

    feat: add back button and improve pathname display in DeanLayout

--- src/app/(authenticated)/dean/layout.tsx
+++ src/app/(authenticated)/dean/layout.tsx
@@ -2,22 +2,40 @@
 
 import React from "react";
 import { useAuth } from "@/hooks/useAuth";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import getCustomPathname from "@/app/utils/getCustomPathname";
+import { ArrowLeftIcon } from "lucide-react";
 
 const DeanLayout = ({ children }: { children: React.ReactNode }) => {
   const { status } = useAuth();
   const pathname = usePathname();
   const customPathname = getCustomPathname(pathname);
+  const router = useRouter();
+
+  // Show back button only on paths with IDs (detail pages or revision pages)
+  const shouldDisplayBackButton =
+    pathname.match(/^\/dean\/proposals\/all-programs\/\d+/) ||
+    pathname.match(/^\/dean\/proposals\/all-syllabi\/\d+/);
 
   if (status === "loading") {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <header className="text-2xl font-bold pt-5 pb-8">{customPathname}</header>
-      <main className="w-full">{children}</main>
+    <div className="h-full flex flex-col">
+      <header className="text-2xl font-bold pt-5 pb-8 flex items-center gap-2 sticky top-0 z-10 bg-white">
+        {shouldDisplayBackButton && (
+          <button
+            onClick={() => router.back()}
+            className="hover:text-gray-600"
+            aria-label="Go back"
+          >
+            <ArrowLeftIcon />
+          </button>
+        )}
+        <span>{customPathname}</span>
+      </header>
+      <main className="w-full grow">{children}</main>
     </div>
   );
 };
